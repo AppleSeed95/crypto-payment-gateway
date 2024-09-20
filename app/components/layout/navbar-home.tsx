340d26619174283eb3d398b6071530db3e67d755
AppleSeed <everlastingfriendship194@gmail.com>
2024-09-20 15:32:56 +0200

    fix : toggle Icon fix

--- app/components/layout/navbar-home.tsx
+++ app/components/layout/navbar-home.tsx
@@ -63,6 +63,7 @@ export default function HomeNavbar() {
                                 aria-label="Toggle theme"
                                 pressed={theme === 'dark'}
                                 onPressedChange={toggleTheme}
+                                className='w-[50px]'
                             >
                                 {theme === 'light' ? (
                                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
